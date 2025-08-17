2e79ccb8cb8a81a4b8f483f0872967576595bca0
kostantin <kandonov2@gmail.com>
2025-08-17 07:32:26 +0200

    initial commit

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 Vestel QA technical challenge
+1. A developer needs to write a function for converting age (a whole number), into life
+   period using the following algorithm:
+- If age is zero, it should return INVALID
+- If age is greater than zero and less than 18, function should return CHILD
+- If age is greater than or equal to 18, function should return ADULT.
+  Define the optimal (effective and efficient) set of boundary test cases to test the
+  function
+
--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -12,20 +12,20 @@ public class BaseClass {
 
     @BeforeMethod
     public void setup() {
-        // Selenium 4.19.1 will auto-manage ChromeDriver via Selenium Manager
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized"); // Maximize window
+        options.addArguments("--start-maximized");
         options.addArguments("--remote-allow-origins=*"); // Allow all origins
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // Global wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.saucedemo.com/");
     }
 
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-            driver.quit(); // Safely close browser
+            driver.quit();
         }
     }
 }
